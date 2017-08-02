3b0f861fe83b8dcb1ddd05e898a893ec790d39a5
yslaem <yslaem@hotmail.com>
2017-08-02 20:22:39 +0200

    Primer commit

---
+++ Hola_mundo/src/Hola_mundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tismen
+ *
+ */
+public class Hola_mundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
